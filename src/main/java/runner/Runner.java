233fdb05266de2fd11ed615a090d8b2a0bd28fa8
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-02-29 00:43:08 +0600

    Create class Runner

---
+++ src/main/java/runner/Runner.java
@@ -0,0 +1,27 @@
+package runner;
+
+import parser.Parser;
+import reader.TextReader;
+import text.CompositeType;
+import text.TextComposite;
+
+public class Runner {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Parser p = new Parser();
+
+        String textString = TextReader.readFileToString("books/cyclone.txt");
+
+        TextComposite text = p.parse(textString, CompositeType.TEXT);
+        String result = text.toPlainString(new StringBuilder());
+
+        System.out.println(textString.equals(result));
+        System.out.print(result);
+
+
+
+
+
+
+    }
+}

ab145f7f463796463d1af025a7977d0db9df3212
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-03-04 15:31:41 +0600

    Modify output

--- src/main/java/runner/Runner.java
+++ src/main/java/runner/Runner.java
@@ -2,7 +2,10 @@ package runner;
 
 import parser.Parser;
 import reader.TextReader;
+import service.TextService;
 import text.TextComposite;
+
+import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
@@ -15,6 +18,19 @@ public class Runner {
         String result = text.toPlainString(new StringBuilder()).toString();
 
         System.out.println(textString.equals(result));
+
+        System.out.println(text.getWords().size());
+        System.out.println(text.getSentences().size());
+        System.out.println(text.getParagraphs().size());
+
+//        for (TextComposite t : text.getParagraphs()) {
+//            System.out.print(t.toPlainString(new StringBuilder()).toString());
+//        }
+
+//        TextService.changeFirstAndLastWordInSentences(text);
+//        System.out.println(text.toPlainString(new StringBuilder()).toString());
+
+
 
     }
 }

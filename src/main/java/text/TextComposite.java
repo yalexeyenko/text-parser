e92973eea58f9ddf1a34dea0fba898e32249e834
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-03-04 15:30:01 +0600

    Create indexOf(), getWords(), getSentences(), getParagraphs() methods. Implement Iterable

--- src/main/java/text/TextComposite.java
+++ src/main/java/text/TextComposite.java
@@ -1,9 +1,17 @@
 package text;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class TextComposite implements Component {
+public class TextComposite implements Component, Iterable<Component> {
+
+    private final static Logger LOG = LoggerFactory.getLogger(TextComposite.class);
+
     private List<Component> components;
     private Type type;
     private int size;
@@ -25,12 +33,76 @@ public class TextComposite implements Component {
         components.add(component);
     }
 
+    public int getIndexOf(Component component) {
+        return this.components.indexOf(component);
+    }
+
     public int size() {
         return components.size();
     }
 
     public List<Component> getComponents() {
         return components;
+    }
+
+    public List<TextComposite> getParagraphs() {
+        List<TextComposite> paragraphsList = new ArrayList<>();
+        fillParagraphsList(paragraphsList);
+        LOG.debug("Paragraphs number: " + paragraphsList.size());
+        return paragraphsList;
+    }
+
+    private void fillParagraphsList(List<TextComposite> paragraphsList) {
+        for (Component component : this.components) {
+            if (component instanceof TextComposite) {
+                TextComposite textComposite = (TextComposite) component;
+                if (textComposite.getType() == Type.PARAGRAPH) {
+                    paragraphsList.add(textComposite);
+                } else {
+                    textComposite.fillParagraphsList(paragraphsList);
+                }
+            }
+        }
+    }
+
+    public List<TextComposite> getSentences() {
+        List<TextComposite> sentencesList = new ArrayList<>();
+        fillSentencesList(sentencesList);
+        LOG.debug("Sentences number: " + sentencesList.size());
+        return sentencesList;
+    }
+
+    private void fillSentencesList(List<TextComposite> sentencesList) {
+        for (Component component : this.components) {
+            if (component instanceof TextComposite) {
+                TextComposite textComposite = (TextComposite) component;
+                if (textComposite.getType() == Type.SENTENCE) {
+                    sentencesList.add(textComposite);
+                } else {
+                    textComposite.fillSentencesList(sentencesList);
+                }
+            }
+        }
+    }
+
+    public List<TextComposite> getWords() {
+        List<TextComposite> wordsList = new ArrayList<>();
+        fillWordsList(wordsList);
+        LOG.debug("Words number: " + wordsList.size());
+        return wordsList;
+    }
+
+    private void fillWordsList(List<TextComposite> wordsList) {
+        for (Component component : this.components) {
+            if (component instanceof TextComposite) {
+                TextComposite textComposite = (TextComposite) component;
+                if (textComposite.getType() == Type.WORD) {
+                    wordsList.add(textComposite);
+                } else {
+                    textComposite.fillWordsList(wordsList);
+                }
+            }
+        }
     }
 
     @Override
@@ -46,6 +118,11 @@ public class TextComposite implements Component {
             component.toPlainString(sb);
         }
         return sb;
+    }
+
+    @Override
+    public Iterator<Component> iterator() {
+        return components.iterator();
     }
 
     public enum Type {

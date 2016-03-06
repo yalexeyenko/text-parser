78780e7ac92d2968b1ea2ca6f907e7cd0776653a
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-03-06 12:03:35 +0600

    Modify output

--- src/main/java/runner/Runner.java
+++ src/main/java/runner/Runner.java
@@ -1,36 +1,16 @@
 package runner;
 
-import parser.Parser;
 import reader.TextReader;
 import service.TextService;
-import text.TextComposite;
-
-import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
 
-        Parser p = new Parser();
-
         String textString = TextReader.readFileToString("books/cyclone.txt");
+        System.out.println(textString);
 
-        TextComposite text = p.parse(textString, TextComposite.Type.TEXT);
-        String result = text.toPlainString(new StringBuilder()).toString();
-
-        System.out.println(textString.equals(result));
-
-        System.out.println(text.getWords().size());
-        System.out.println(text.getSentences().size());
-        System.out.println(text.getParagraphs().size());
-
-//        for (TextComposite t : text.getParagraphs()) {
-//            System.out.print(t.toPlainString(new StringBuilder()).toString());
-//        }
-
-//        TextService.changeFirstAndLastWordInSentences(text);
-//        System.out.println(text.toPlainString(new StringBuilder()).toString());
-
-
+        String result2 = TextService.changeFirstAndLastWordInSentences(textString);
+        System.out.println(result2);
 
     }
 }
